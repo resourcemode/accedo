240edea79c72d36172e9a8635491c954fbec2356
Michael Favila <resourcemode@yahoo.com>
2016-03-30 23:31:58 +0800

    nginx congig sample

--- resources/views/client/history/index.blade.php
+++ resources/views/client/history/index.blade.php
@@ -2,6 +2,5 @@
   <button id="loader" class="btn btn-lg btn-info"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>Loading...</button>
 </div>
 <div class="clearfix"></div>
-<div class="owl-carousel owl-theme text-center" id="carousel"></div>
-
+<div class="owl-carousel owl-theme text-center" id="carousel"></div><br/>
 
